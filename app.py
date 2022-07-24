240a7ed1758f11d7e40f25604a9015370c49adda
Zac <zac@identityworksllc.com>
2022-07-24 16:13:58 -0500

    Added controls for multiple blinds at once

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+from datetime import datetime
 from flask import Flask, request, jsonify
-# import am43
+import am43
 import sqlite3
 import os
+from subprocess import Popen
+import json
+import threading
+import time
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.run(host='0.0.0.0')
 
 
 try:
@@ -24,6 +31,41 @@ conn.execute('''CREATE TABLE IF NOT EXISTS "blinds" (
 
 conn.close()
 
+
+def ping_blind(mac_address=None, blind=None):
+    conn = sqlite3.connect('am43.db')
+    try:
+        if blind is None and mac_address is None:
+            blinds_list = get_blinds_from_db()
+            mac_list = []
+            blinds_list = json.loads(blinds_list)
+            for blind in blinds_list:
+                mac_list.append(str(blind['mac_address']))
+            print(mac_list)
+            blinds = am43.search(*mac_list)
+        else:
+            blinds = [am43.search(mac_address)]
+        print(blinds)
+        for blind in blinds:
+            properties = blind.get_properties()
+            # print(blind._device.addr)
+            conn.execute("UPDATE blinds SET battery=?, position=?, light=? WHERE mac_address=?", (properties.battery, properties.position, properties.light, blind._device.addr.upper()))
+            conn.commit()
+            msg="Successfully updated blind in database"
+            print(msg)
+            print(properties)
+            blind.disconnect()
+    except Exception as e:
+        msg="Error updating blind in database, performing rollback"
+        print(msg)
+        print(e)
+        conn.rollback()
+    finally:
+        conn.close()
+        print("Finished updating blind db")
+        return msg
+        
+
 def input_blind_to_db(name, mac_address, battery, position, light):
     try:
         conn = sqlite3.connect('am43.db')
@@ -36,39 +78,81 @@ def input_blind_to_db(name, mac_address, battery, position, light):
         print(msg)
         conn.rollback()
     finally:
-        return msg
         conn.close()
+        return msg
+        
 
 def get_blinds_from_db():
     try:
         conn = sqlite3.connect('am43.db')
-        cursor = conn.execute("SELECT * FROM blinds")
-        blinds = []
-        for row in cursor:
-            blinds.append(row)
-        return blinds
+        conn.row_factory = sqlite3.Row
+        blinds = conn.execute("SELECT * FROM blinds").fetchall()
+        # cursor = conn.execute("SELECT * FROM blinds")
+        # blinds = []
+        # for row in cursor:
+        #     blinds.append(row)
+        return json.dumps([dict(ix) for ix in blinds])
     except:
         print("Error getting blinds from database")
         return None
     finally:
         conn.close()
 
-#may not be needed or need to be updated?
-def update_blind_in_db(name, mac_address, battery, position, light):
+def set_blind_position(mac_address, position):
+    conn = sqlite3.connect('am43.db')
     try:
-        conn = sqlite3.connect('am43.db')
-        conn.execute("UPDATE blinds SET name=?, mac_address=?, battery=?, position=?, light=? WHERE name=?", (name, mac_address, battery, position, light, name))
+        blind = am43.search(mac_address)
+        print(blind)
+        blind.set_position(int(position))
+        msg="Successfully set blind position"
+        print(msg)
+        conn.execute("UPDATE blinds SET position=? WHERE mac_address=?", (position, mac_address))
         conn.commit()
-        msg="Successfully updated blind in database"
+        print("Successfully updated blind in database")
+    except Exception as e:
+        msg="Error setting blind position"
         print(msg)
-    except:
-        msg="Error updating blind in database, performing rollback"
-        print(msg)
-        conn.rollback()
+        print(e)
     finally:
-        return msg
         conn.close()
+        blind.disconnect()
+        return msg
 
+def set_all_blinds_position(position):
+    start_time = time.time()
+    blinds_list = get_blinds_from_db()
+   
+    mac_list = []
+    blinds_list = json.loads(blinds_list)
+    for blind in blinds_list:
+        mac_list.append(str(blind['mac_address']))
+    print(mac_list)
+    try:
+        blinds = am43.search(*mac_list)
+        for blind in blinds:
+            conn = sqlite3.connect('am43.db') 
+            try:  
+                blind.set_position(int(position))
+                msg="Successfully set blind position"
+                print(msg)
+                conn.execute("UPDATE blinds SET position=? WHERE mac_address=?", (position, blind._device.addr))
+                conn.commit()
+                print("Successfully updated blind in database")
+            except Exception as e:
+                msg="Error setting blind position"
+                print(msg)
+                print(e)
+            finally:
+                conn.close()
+                blind.disconnect()
+    except Exception as e:
+        print("Error when trying to set all blinds position")
+        print(e)
+        return False
+    print("Total time: " + str(round((time.time() - start_time), 2)) + " seconds")
+    return True
+    
+# ping_thread = threading.Timer(30, ping_blind, [request.json['mac_address']])
 
 @app.route('/')
 def index():
@@ -76,16 +160,53 @@ def index():
 
 @app.route('/blinds', methods=['GET'])
 def get_blinds():
-    return jsonify(get_blinds_from_db())
+    return get_blinds_from_db()
 
 @app.route('/blinds', methods=['POST'])
 def add_blind():
-    if not request.args or not 'mac_address' in request.args or not 'name' in request.args:
-        print(request.args)
+    if not request.json or not 'mac_address' in request.json or not 'name' in request.json:
+        print(request.json)
         return "Invalid Request", 400
 
-    input_blind_to_db(request.args['name'], request.args['mac_address'], 0, 0, 0)
-    return jsonify(get_blinds_from_db()), 201
+    input_blind_to_db(request.json['name'], request.json['mac_address'], 0, 0, 0)
+    ping_blind(request.json['mac_address'])
+    return get_blinds_from_db(), 201
+
+@app.route('/blinds/set', methods=['POST'])
+def set_position():
+    for arg in request.json:
+        print(arg)
+    print(request.json)
+    if not request.json or not 'position' in request.json and not 'mac_address' in request.json:
+        print(request.json)
+        return "Invalid Request", 400
+    elif not 'mac_address' in request.json and 'position' in request.json:
+        set_all_blinds_position(request.json['position'])
+        ping_thread = threading.Timer(60, ping_blind)
+        ping_thread.start()
+        print("thread started, waiting to ping")
+        return get_blinds_from_db(), 201
+    else:
+        set_blind_position(request.json['mac_address'], request.json['position'])
+        ping_thread = threading.Timer(30, ping_blind, [request.json['mac_address']])
+        ping_thread.start()
+        print("thread started, waiting to ping")
+        
+        # ping_blind(request.json['mac_address'])
+        return get_blinds_from_db(), 201
+
+@app.route('/blinds/update', methods=['GET'])
+def force_update():
+    print("printing args")
+    if request.json and 'mac_address' in request.json:
+        print(request.json['mac_address'])
+        # for arg in request.args:
+        print("pinging " + request.json['mac_address'])
+        ping_blind(request.json['mac_address'])
+    else:
+        print("pinging all")
+        ping_blind()
+    return get_blinds_from_db(), 201
 
 @app.errorhandler(404)
 def page_not_found(e):
